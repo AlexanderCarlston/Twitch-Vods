a93e0a5f133c43bde924c4e7ffd8983a259578ba
alekscarlston@yahoo.com <alekscarlston@gmail.com>
2019-12-04 20:14:41 -0700

    Add example page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppNavComponent } from './components/app-nav/app-nav.component';
 import { DiscoveryPageComponent } from './pages/discovery-page/discovery-page.component';
 import { VideoCardComponent } from './components/video-card/video-card.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { VideoCardComponent } from './components/video-card/video-card.component
     AppNavComponent,
     DiscoveryPageComponent,
     VideoCardComponent,
+    GameCardComponent,
   ],
   imports: [
     BrowserModule,
