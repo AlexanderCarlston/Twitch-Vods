fddcedbe76b2b73672f15519ebb6867a1d5aff90
alekscarlston@yahoo.com <alekscarlston@gmail.com>
2019-12-07 19:45:42 -0700

    Add functionality for videos-page

--- src/app/components/video-card/video-card.component.ts
+++ src/app/components/video-card/video-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'video-card',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-card.component.scss']
 })
 export class VideoCardComponent implements OnInit {
-
+  @Input() video: object;ß
   constructor() { }
 
   ngOnInit() {
+    console.log(this.video)
   }
 
+  validImgSrc(src: string) {
+    console.log(src.replace('{width}', '180').replace('{height}', '250'))
+    return src.replace('%{width}', '440').replace('%{height}', '248');
+  }
 }
